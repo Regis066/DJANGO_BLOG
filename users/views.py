9fd98dd490838910445fa73769e835c354395f59
Regis066 <mugisharegis72@gmail.com>
2023-09-24 16:27:49 +0200

    added new features

--- users/views.py
+++ users/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']  #or form.cleaned_data.get('username')
-            messages.success(request, f"Account created for {username}!")
-            return redirect('blog-home')
+            messages.success(request, f"Your Accounthas been created! Now Log In!")
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'user/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'user/profile.html')
 
 
 
