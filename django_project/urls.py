360cc2270d652da0142b32127017dc022775bb14
Regis066 <mugisharegis72@gmail.com>
2023-09-26 20:40:54 +0200

    Added password reset feature

--- django_project/urls.py
+++ django_project/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     path("profile/", user_views.profile , name='profile'),
     path("login/", auth_views.LoginView.as_view(template_name = "user/login.html") , name='login'),
     path("logout/", auth_views.LogoutView.as_view(template_name = "user/logout.html") , name='logout'),
+    path("password-reset/", auth_views.PasswordResetView.as_view(template_name = "user/password_reset.html") , name='password-reset'),
+    path("password-reset/", auth_views.PasswordResetView.as_view(template_name = "user/password_reset.html") , name='password-reset'),
     
 ]
 
