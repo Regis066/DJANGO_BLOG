2dc94c8c1d1084737966fab5337a00168f374d1e
Regis066 <mugisharegis72@gmail.com>
2023-09-26 21:46:37 +0200

    Added new things

--- django_project/urls.py
+++ django_project/urls.py
@@ -30,7 +30,9 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name = "user/login.html") , name='login'),
     path("logout/", auth_views.LogoutView.as_view(template_name = "user/logout.html") , name='logout'),
     path("password-reset/", auth_views.PasswordResetView.as_view(template_name = "user/password_reset.html") , name='password-reset'),
-    path("password-reset/", auth_views.PasswordResetView.as_view(template_name = "user/password_reset.html") , name='password-reset'),
+    path("password-reset/done", auth_views.PasswordResetDoneView.as_view(template_name = "user/password_reset_done.html") , name='password-reset-done'),
+    path("password-reset/confirm/<uid64>/<token>", auth_views.PasswordResetConfirmView.as_view(template_name = "user/password_reset_confirm.html") , name='password-reset-confirm'),
+    path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name = "user/password_reset_complete.html") , name='password-reset-complete'),
     
 ]
 
